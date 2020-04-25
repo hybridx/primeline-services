f661ee7def6aac342d8d7b2e6cb30793cea7426b
hybridx <hybridx18@gmail.com>
2020-04-25 11:18:02 +0530

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Primeline-Services
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+### Development server
+- Run development server
+
+```sh
+npm run dev
+```
+
+### Build
+
+```sh
+npm run build
+```
+
+## Run tests
+
+```sh
+npm run build
+npm run test
+```
+
+## 🤝 Contributors
+
+👤 **Deepesh Nair** [@hybridx](https://github.com/hybridx)
---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/app.js',
+    },
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 1234,
+    },
+    optimization : {
+        minimizer: [new TerserPlugin()],
+        usedExports: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-object-rest-spread']
+                  },
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin, 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                  ],
+            },
+            {
+                test: /\.(svg|png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'img',
+                            name: '[name].[ext]',
+                            esModule: false,
+                        },
+                    }
+                ]
+            },        
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.scss', '.css'],
+    },
+    plugins: [
+        new Dotenv(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            chunks: ['app'],
+            template: './src/index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+            },
+        }),
+        new MiniCssExtractPlugin(),
+    ]
+};
